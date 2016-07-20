7161549467491f4c1da11d05ecf4b8b98344db9c
Adam Siwiec <brickmaster01@gmail.com>
2016-07-19 22:51:59 -0500

    Added support for openshift

--- tag/tag/settings.py
+++ tag/tag/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-print (BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -23,9 +21,9 @@ print (BASE_DIR)
 SECRET_KEY = 'osx*hq-(zkj=&&o!d%u&e-=^1%6y0gvsu9d)-#39k$i1(1+5&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = '/profile'
 
@@ -78,13 +76,18 @@ WSGI_APPLICATION = 'tag.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import urlparse
+db_url = urlparse.urlparse(os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL'))
+
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['OPENSHIFT_APP_NAME'],
+    'USER': db_url.username,
+    'PASSWORD': db_url.password,
+    'HOST': db_url.hostname,
+    'PORT': db_url.port,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -121,11 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
-
-MEDIA_ROOT = "/Users/adamsiwiec/Desktop/Tag/tag/tags/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'tags/images')
+# MEDIA_ROOT = "/Users/adamsiwiec/Desktop/Tag/tag/tags/images/"
 MEDIA_URL = "/images/"
--- tag/tag/wsgi.py
+++ tag/tag/wsgi.py
@@ -1,16 +1,24 @@
-"""
-WSGI config for tag project.
+#!/usr/bin/python
+import os, sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tag.settings'
 
-import os
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tag.settings")
-
 application = get_wsgi_application()
