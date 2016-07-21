8897a1d67caef3b429185cbb983277d021bea55b
Adam Siwiec <brickmaster01@gmail.com>
2016-07-21 17:16:17 -0500

    Removed ImageKit support

--- tags/models.py
+++ tags/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
+
 
 
 
