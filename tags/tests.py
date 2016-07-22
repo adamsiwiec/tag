e8f0a7fe836ce4abf046e3a7e823f54001fa1146
Adam Siwiec <brickmaster01@gmail.com>
2016-07-22 09:50:29 -0500

    Added few test cases

--- tags/tests.py
+++ tags/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from tag.models import *
 # Create your tests here.
+
+class UserCreationTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username = "testcase", password = "password", first_name = "Adam", last_name = "Siwiec", email = "brick@gmail.com")
+
+    def test_user_is_created(self):
+        """User is created perfectly"""
+        test = User.objects.get(username = "testcase")
+        self.assertEqual(test.first_name, "Adam")
+
+class HomepageTestCase(TestCase):
+    def homepage_is_up(self):
+        """Homepage runs succesfully"""
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
