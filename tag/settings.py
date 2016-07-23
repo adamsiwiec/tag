d3141bc0c3c7b513bc32d273362c6e136ec51de9
Adam Siwiec <brickmaster01@gmail.com>
2016-07-22 19:56:41 -0500

    Added AWS S3 as static storage

--- tag/settings.py
+++ tag/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tags',
     'crispy_forms',
-    
+    'storages'
+
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
@@ -130,7 +131,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tags/images')
 # MEDIA_ROOT = "/Users/adamsiwiec/Desktop/Tag/tag/tags/images/"
 MEDIA_URL = "/images/"
@@ -145,3 +145,26 @@ DATABASES['default'].update(db_from_env)
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'Cache-Control': 'max-age=94608000',
+    }
+
+AWS_STORAGE_BUCKET_NAME = 'tagyourit'
+AWS_ACCESS_KEY_ID = 'AKIAIDO23IONYN4UVTDQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
